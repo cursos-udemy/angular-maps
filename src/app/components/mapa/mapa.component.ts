8e16c15d7f8eff7d99e6adf83a273dc63893f4e1
gwfernandez <gwfernandez@gmail.com>
2018-11-22 16:28:42 -0300

    agrego snackbar para noticaciones

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,47 +1,55 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
-import { Marcador } from '../../classes/marcador.class';
+import { Component, OnInit } from "@angular/core";
+import { Marcador } from "../../classes/marcador.class";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
-  selector: 'app-mapa',
-  templateUrl: './mapa.component.html',
-  styles: [`
-  agm-map {
-    height: 450px;
-  }
-  `]
+  selector: "app-mapa",
+  templateUrl: "./mapa.component.html",
+  styles: [
+    `
+      agm-map {
+        height: 450px;
+      }
+    `
+  ]
 })
 export class MapaComponent implements OnInit {
-
-  public marcadores:Marcador[];
+  public marcadores: Marcador[];
 
   public lat = -34.688003;
   public lng = -58.729416;
 
-  constructor() { 
+  constructor(public snackBar: MatSnackBar) {
     this.marcadores = [];
     // let nuevoMarcador = new Marcador(-34.688003, -58.729416);
     // this.marcadores.push (nuevoMarcador);
-    if (localStorage.getItem("marcadores") ){
+    if (localStorage.getItem("marcadores")) {
       this.marcadores = JSON.parse(localStorage.getItem("marcadores"));
     }
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  public agregarMarcador (evento) {
+  public agregarMarcador(evento) {
     let nuevoMarcador = new Marcador(evento.coords.lat, evento.coords.lng);
-    this.marcadores.push (nuevoMarcador);
+    this.marcadores.push(nuevoMarcador);
+    this.openSnackBar("Marcador Agregado", "Cerrar");
     this.saveStorage();
   }
 
-  public borrarMarcador(index:number) {
+  public borrarMarcador(index: number) {
     this.marcadores.splice(index, 1);
+    this.openSnackBar("Marcador Eliminado", "Cerrar");
     this.saveStorage();
+  }
+
+  private openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 
   private saveStorage() {
-    localStorage.setItem("marcadores",JSON.stringify(this.marcadores));
+    localStorage.setItem("marcadores", JSON.stringify(this.marcadores));
   }
-
 }
